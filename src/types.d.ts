1c511d191ad8f59c9ec61f979635bb15f78489a8
matias <matias.saantiago@gmail.com>
2022-09-22 20:11:34 -0300

    uno

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/indent */
+export type Weather = 'sunny' | 'rainy' | 'cloudy' | 'windy' | 'stormy'
+export type Visibility = 'great' | 'good' | 'ok' | 'poor'
+
+export interface DiaryEntry {
+    id: number
+    date: string
+    weather: Weather
+    visibility: Visibility
+    comment: string
+}
