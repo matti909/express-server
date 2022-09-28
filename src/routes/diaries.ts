2e2fa1ef56029dff194641b00064299ddc85ca83
matias <matias.saantiago@gmail.com>
2022-09-28 11:25:01 -0300

    doss

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,9 +1,10 @@
 import express from 'express' // ESmodules
+import * as diaryServices from '../services/diaryServices'
 
 const router = express.Router()
 
 router.get('/', (_req, res) => {
-  res.send('Fetching all entry diaries')
+  res.send(diaryServices.getEntriesWithoutSensitiveInfo())
 })
 
 router.post('/', (_req, res) => {
--- src/services/diaryServices.ts
+++ src/services/diaryServices.ts
@@ -1,8 +1,22 @@
-import { DiaryEntry } from '../types'
+import { DiaryEntry, NonSensitiveInfoDiaryEntry } from '../types'
 import diaryData from './diaries.json'
 
 const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
 export const getEntries = (): DiaryEntry[] => diaries
 
-export const addEntry = (): undefined => undefined
+export const findeById = (id: number): DiaryEntry | undefined => {
+	const entry = diaries.find(d => d.id === id)
+	return entry
+}
+
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => {
+    return {
+      id,
+      date,
+      weather,
+      visibility
+    }
+  })
+}
